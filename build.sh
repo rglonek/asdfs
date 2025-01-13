fdb8f40952d0e1c21eaa131d2f2387346ad0c559
Robert Glonek <rglonek@aerospike.com>
2025-01-12 16:53:45 -0800

    adjust test build scripts

--- build.sh
+++ build.sh
@@ -3,20 +3,21 @@
 # run build
 docker run -it --rm -v ./:/mnt/ gofuse bash -c "cd /mnt && /root/go/bin/go build -o mount.asdfs ."
 # run test
-aerolab cluster create -n asdfs -o aerospike.conf -v 8.0.0.0-rc1 --privileged --no-autoexpose
+aerolab config backend -t docker
+aerolab cluster create -n asdfs -o aerospike.conf -v 8.0.0.0-rc3 --privileged --no-autoexpose
 aerolab files upload -n asdfs mount.asdfs /usr/sbin/mount.asdfs
 aerolab files upload -n asdfs asdfs.yaml /etc/asdfs.yaml
 aerolab attach shell -n asdfs -- bash -c "apt update && apt -y install fuse3"
 aerolab attach shell -n asdfs -- mkdir /test
 aerolab roster apply -n asdfs -m test
-aerolab attach shell -n asdfs --detach -- mount -t asdfs /etc/asdfs.yaml /test
+aerolab attach shell -n asdfs --detach -- mount -t asdfs /etc/asdfs.yaml /test --debug
 while true; do
     echo "Press ENTER to rededploy"
     read
     aerolab attach shell -n asdfs -- umount /test
     docker run -it --rm -v ./:/mnt/ gofuse bash -c "cd /mnt && /root/go/bin/go build -o mount.asdfs ."
     aerolab files upload -n asdfs mount.asdfs /usr/sbin/mount.asdfs
-    aerolab attach shell -n asdfs --detach -- mount -t asdfs /etc/asdfs.yaml /test
+    aerolab attach shell -n asdfs --detach -- mount -t asdfs /etc/asdfs.yaml /test --debug
 done
 # aerolab cluster destroy -f -n asdfs
 # mount -t asdfs /etc/asdfs.yaml /test
